02b0104cdaf09af7571eb7aa51035f7f5ded45ec
Saumitra Paul <skpaul@gmail.com>
2023-04-19 20:41:14 +0600

    sdfdfg

--- src/UniqueCodeGenerator.php
+++ src/UniqueCodeGenerator.php
@@ -12,7 +12,7 @@
             $charCodes = [65,66,67,68,69,70,71,72,74,75,77,78,80,81,82,83,84,85,86,88,89,90];
             $code ='';
             for ($i=1; $i <= $length; $i++){ 
-                $random_number = rand(0,22); 
+                $random_number = rand(0,21); 
                 $code .= chr($charCodes[$random_number]);
             }
 
